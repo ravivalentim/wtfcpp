17f158e7ffbb9d626ed1d2e011d86f4b3f92e5f6
Ravi Valentim <ravivalentim.xor@gmail.com>
2023-10-29 20:20:24 -0300

    modified:   wtf.cpp

--- wtf.cpp
+++ wtf.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <vector>
 
 class {
 public:
     void log(const char* log) {
         std::cout << log << std::endl;
+    }
+    template<typename T>
+    void log(const std::vector<T>& array) {
+        std::cout << "{ ";
+        for (int i = 0; T element : array) {
+            std::cout << element;
+            if (i < array.size() - 1) 
+                std::cout << ", ";
+            i++;
+        }
+        std::cout << " }";
     }
 } console;
 
@@ -18,6 +30,9 @@ public:
 
 int main() {
 
+    std::vector<int> a { 8, 5, 2, 3, 9, 8, 7};
+
+    console.log(a);
     console.log("Hello, world");
     System.out.println("Hello, world");
 
